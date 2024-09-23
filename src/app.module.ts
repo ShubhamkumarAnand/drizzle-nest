dd6354506f1869a22822cba62dcd4f11f4bfb4ce
imskanand <the.anand3099@gmail.com>
2024-09-23 23:02:47 +0530

    feat: drizzle-config, schema & migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
-import { Module } from "@nestjs/common";}
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: []
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UsersModule,
+  ],
+  controllers: [],
 })
-
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { DATABASE_CONNECTION } from './database-connection';
+
+@Module({
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      useFactory: (configService: ConfigService) => {
+        const pool = new Pool({
+          connectionString: configService.getOrThrow('DATABASE_URL'),
+        });
+        return drizzle(pool, { schema: {} });
+      },
+      inject: [ConfigService],
+    },
+  ],
+})
+export class DatabaseModule {}
